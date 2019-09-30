67c23d63e2cb55b572ef11bad9809bf30ceb7088
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-09-30 13:38:37 +0200

    Add styles from UI designer

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.scss';
+import 'animate.css/animate.min.css';
 
 const Root = () => (
 	<App />
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
-
-
