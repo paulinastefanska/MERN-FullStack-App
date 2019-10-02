5a0b2c9e90f14137c4531e17ece6c4313082cd9f
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-02 15:29:42 +0200

    Add loader

--- client/src/components/features/Posts/Posts.js
+++ client/src/components/features/Posts/Posts.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 
 import PostsList from '../PostsList/PostsList';
+import Spinner from '../../common/Spinner/Spinner';
 
 class Posts extends React.Component {
   
@@ -11,10 +12,11 @@ class Posts extends React.Component {
   }
 
   render() {
-  	const { posts } = this.props;
+  	const { posts, request } = this.props;
 
     return (
       <div>
+        {request.pending && <Spinner />}
         <PostsList posts={posts} />
       </div>
     );
--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -7,24 +7,39 @@ const createActionName = name => `app/${reducerName}/${name}`;
 
 /* SELECTORS */
 
-export const getPosts = ({ posts }) => posts;
-export const getPostsCounter = ({ posts }) => posts.lenght;
+export const getPosts = ({ posts }) => posts.data;
+export const getPostsCounter = ({ posts }) => posts.data.lenght;
+export const getRequest = ({ posts }) => posts.request;
 
 /* ACTIONS */
 
 export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const START_REQUEST = createActionName('START_REQUEST');
+export const END_REQUEST = createActionName('END_REQUEST');
+
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+export const startRequest = () => ({ type: START_REQUEST });
+export const endRequest = () => ({ type: END_REQUEST });
 
 /* INITIAL STATE */
 
-const initialState = [];
+const initialState = {
+  data: [],
+  request: {
+    pending: false,
+  },
+};
 
 /* REDUCER */
 
 export default function reducer(statePart = initialState, action = {}) {
   switch (action.type) {
   	case LOAD_POSTS:
-      return [ ...action.payload ];
+      return { ...statePart, data: action.payload };
+    case START_REQUEST:
+      return { ...statePart, request: { pending: true } };
+    case END_REQUEST:
+      return { ...statePart, request: { pending: false } };
     default:
       return statePart;
   }
@@ -34,14 +49,17 @@ export default function reducer(statePart = initialState, action = {}) {
 
 export const loadPostsRequest = () => {
   return async dispatch => {
-
+    
+    dispatch(startRequest());
     try {
 
       let res = await axios.get(`${API_URL}/posts`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
       dispatch(loadPosts(res.data));
+      dispatch(endRequest());
 
     } catch(e) {
-      console.log(e.message);
+      dispatch(endRequest());
     }
 
   };
