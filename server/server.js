06238cdbe9376219c1c71ac6ca1148cb90a4e02f
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-03 13:48:04 +0200

    Edit server

--- server/server.js
+++ server/server.js
@@ -4,17 +4,13 @@ const config = require('./config');
 
 const app = express();
 
+// import routes
+const postRoutes = require('./routes/post.routes');
+
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
-app.get('/api/posts', (req, res) => {
-  const data = [
-    { id: '1adfasf', title: 'Lorem Ipsum', content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit.' },
-    { id: '2evxc34', title: 'Lorem Ipsum II', content: 'Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit. Neque porro quisquam est qui dolorem ipsum quia dolor sit amet consectetur, adipisci velit.' },
-  ]
-  res.json(data);
-});
+app.use('/api', postRoutes);
 
 app.listen(config.PORT, function(){
   console.log('Server is running on port:', config.PORT); 
