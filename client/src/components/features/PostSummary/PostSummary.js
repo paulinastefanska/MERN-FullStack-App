9c3ae53f8885e458fce2d062024b5bf6b2553ed7
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-03 15:25:13 +0200

    Add link to single post

--- client/src/components/features/PostSummary/PostSummary.js
+++ client/src/components/features/PostSummary/PostSummary.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import Button from '../../common/Button/Button';
 import SmallTitle from '../../common/SmallTitle/SmallTitle';
@@ -12,7 +13,7 @@ const PostSummary = ({ id, title, content }) => (
     <SmallTitle>{title}</SmallTitle>
     <HtmlBox>{cutText(content, 200)}</HtmlBox>
     <Button variant="primary">
-      Read more
+      <Link to={`/posts/${id}`}>Read more</Link>
     </Button>
   </article>
 );
