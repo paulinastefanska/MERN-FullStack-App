f288baabf2231dd2a1bea90763c0468b445abdf2
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-04 13:44:33 +0200

    Add new endoint

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post.model');
+const uuid = require('uuid');
 
 // get all posts
 exports.getPosts = async (req, res) => {
@@ -19,5 +20,23 @@ exports.getSinglePost = async (req, res) => {
     } catch(err) {
       res.status(500).json(err);
     }
+
+};
+
+// add new post
+exports.addPost = async function (req, res) {
+
+  try {
+    const { title, author, content } = req.body;
+
+    let newPost = new Post(req.body);
+    newPost.id = uuid();
+
+    postSaved = await newPost.save();
+    res.status(200).json(postSaved);
+
+  } catch(err) {
+    res.status(500).json(err);
+  }
 
 };
