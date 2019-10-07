4eb48bd177f5303c41f121af05904bfc8cb6dbe0
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-07 13:22:32 +0200

    Add new reducer

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -11,6 +11,7 @@ export const getPosts = ({ posts }) => posts.data;
 export const getPostsCounter = ({ posts }) => posts.data.length;
 export const getRequest = ({ posts }) => posts.request;
 export const getSinglePost = ({ posts }) => posts.singlePost;
+export const getPages = ({ posts }) => Math.ceil(posts.amount / posts.postsPerPage);
 
 /* ACTIONS */
 
@@ -20,6 +21,7 @@ export const START_REQUEST = createActionName('START_REQUEST');
 export const END_REQUEST = createActionName('END_REQUEST');
 export const ERROR_REQUEST = createActionName('ERROR_REQUEST');
 export const RESET_REQUEST = createActionName('RESET_REQUEST');
+export const LOAD_POSTS_PAGE = createActionName('LOAD_POSTS_PAGE');
 
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
 export const loadSinglePost = payload => ({payload, type: LOAD_SINGLE_POST});
@@ -27,6 +29,7 @@ export const startRequest = () => ({ type: START_REQUEST });
 export const endRequest = () => ({ type: END_REQUEST });
 export const errorRequest = error => ({ error, type: ERROR_REQUEST });
 export const resetRequest = () => ({type: RESET_REQUEST});
+export const loadPostsByPage = payload => ({ payload, type: LOAD_POSTS_PAGE });
 
 /* INITIAL STATE */
 
@@ -38,6 +41,9 @@ const initialState = {
     success: null,
   },
   singlePost: [],
+  amount: 0,
+  postsPerPage: 10,
+  presentPage: 1,
 };
 
 /* REDUCER */
@@ -56,6 +62,14 @@ export default function reducer(statePart = initialState, action = {}) {
       return { ...statePart, request: { pending: false, error: action.error, success: false } };
     case RESET_REQUEST:
       return {...statePart, request: { pending: false, error: null, success: null } };
+    case LOAD_POSTS_PAGE:
+      return {
+        ...statePart,
+        postsPerPage: action.payload.postsPerPage,
+        presentPage: action.payload.presentPage,
+        amount: action.payload.amount,
+        data: [...action.payload.posts],
+      };
     default:
       return statePart;
   }
@@ -107,6 +121,37 @@ export const addPostRequest = (post) => {
 
       const res = await axios.post(`${API_URL}/posts`, post);
       await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      dispatch(endRequest());
+
+    } catch(e) {
+      dispatch(errorRequest(e.message));
+    }
+
+  };
+};
+
+export const loadPostsByPageRequest = (page) => {
+  return async dispatch => {
+
+    dispatch(startRequest());
+    try {
+
+      const postsPerPage = 10;
+
+      const startAt = (page - 1) * postsPerPage;
+      const limit = postsPerPage;
+
+      let res = await axios.get(`${API_URL}/posts/range/${startAt}/${limit}`);
+      await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+
+      const payload = {
+        posts: res.data.posts,
+        amount: res.data.amount,
+        postsPerPage,
+        presentPage: page,
+      };
+
+      dispatch(loadPostsByPage(payload));
       dispatch(endRequest());
 
     } catch(e) {
