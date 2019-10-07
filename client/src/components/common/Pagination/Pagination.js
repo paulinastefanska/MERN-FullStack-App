063fe07c8c3a13a8eeec1d27fda7f167a9a398e6
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-07 11:57:56 +0200

    Edit render function

--- client/src/components/common/Pagination/Pagination.js
+++ client/src/components/common/Pagination/Pagination.js
@@ -4,15 +4,34 @@ import PropTypes from 'prop-types';
 import './Pagination.scss';
 
 class Pagination extends React.Component {
+	
+	state = {
+	  presentPage: this.props.initialPage || 1
+	}
 
   render() {
 
-    return (
-      <div className="pagination">
+  const { pages, onPageChange } = this.props;
+  const { presentPage } = this.state;
+  const { changePage } = this;
 
-      </div>
-    );
-  }
+  return (
+    <div className="pagination">
+      <ul className="pagination__list">
+
+        {[...Array(pages)].map((el, page) =>
+        <li
+            key={++page}
+            onClick={() => { changePage(page) }}
+            className={`pagination__list__item${((page) === presentPage) ? ' pagination__list__item--active' : ''}`}>
+            {page}
+        </li>
+        )}
+
+      </ul>
+    </div>
+  );
+}
 
 }
 
