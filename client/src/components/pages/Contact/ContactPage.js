feb9cad00aea030a5f806d01a21c4e9107b5c3ed
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-10 09:58:28 +0200

    Add address to contact page

--- client/src/components/pages/Contact/ContactPage.js
+++ client/src/components/pages/Contact/ContactPage.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import PageTitle from '../../common/PageTitle/PageTitle';
+
 const ContactPage = () => (
   <div>
-    <h1>Contact</h1>
-  </div>
+	  <PageTitle>Contact</PageTitle>
+	  <p><b>Your Favourite Blooger Ltd.</b></p>
+		<p><i>9432 Division Street</i></p>
+		<p><i>Stone Mountain, GA 30083</i></p>
+		<p><b>Mobile Number: 207-914-3411</b></p>
+	</div>
 );
 
 export default ContactPage;
