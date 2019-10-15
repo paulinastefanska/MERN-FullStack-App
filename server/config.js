6b585bf64cbfdb3b0b1c202dfa97fb6289fd91c6
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-15 17:57:35 +0200

    Hide database login

--- server/config.js
+++ server/config.js
@@ -1,4 +1,4 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
-  DB: 'mongodb+srv://blogApp:blogAppPassword@cluster0-0avtn.mongodb.net/test?retryWrites=true&w=majority',
+  DB: process.env.DB,
 };
