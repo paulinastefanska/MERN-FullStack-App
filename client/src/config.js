ddaea1b9b057efbed32c4521a11c40f0807e5963
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-15 14:58:04 +0200

    Fix config

--- client/src/config.js
+++ client/src/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://localhost:8000/api';
-export const BASE_URL = 'http://localhost:3000/${location.pathname}';
+export const API_URL = '/api';
+export const BASE_URL = 'https://fullstack-app-blog.herokuapp.com';
