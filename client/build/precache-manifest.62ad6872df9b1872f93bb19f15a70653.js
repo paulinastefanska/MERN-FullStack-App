4285003d79a4b1aad6f6623955b39c0aeee91a80
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-10-15 15:44:05 +0200

    Remove production /build

---
+++ client/build/precache-manifest.62ad6872df9b1872f93bb19f15a70653.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "013f00d37cd97af877a94d4ab2ed8514",
+    "url": "/index.html"
+  },
+  {
+    "revision": "497bef4c1798d6100829",
+    "url": "/static/css/2.d109cac1.chunk.css"
+  },
+  {
+    "revision": "f60f20e3d4ccdc9c5fe1",
+    "url": "/static/css/main.46d1b6da.chunk.css"
+  },
+  {
+    "revision": "497bef4c1798d6100829",
+    "url": "/static/js/2.18bd50c9.chunk.js"
+  },
+  {
+    "revision": "f60f20e3d4ccdc9c5fe1",
+    "url": "/static/js/main.00985df3.chunk.js"
+  },
+  {
+    "revision": "2fde81e2cfcf8d15a02d",
+    "url": "/static/js/runtime-main.ea92a0de.js"
+  }
+]);
